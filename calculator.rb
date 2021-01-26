a35d74cd5bfd7005d13e04c00c5d0fa47496133b
Nicholas Degrate <nicholasdegrate@gmail.com>
2021-01-26 05:07:49 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,9 @@
 # Add your variables here
 first_number = 19
 second_number = 10
-def calculator(first_number= 1, second_number)
-  sum = first_number.to_i + second_number.to_i
-end
+
+sum = first_number.to_i + second_number.to_i
+
 
 def calculator2(first_number = 1, second_number)
 

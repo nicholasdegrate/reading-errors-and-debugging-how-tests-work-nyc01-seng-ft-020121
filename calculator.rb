4efe86145b646e892da6dae9cfbb7cea1567d317
Nicholas Degrate <nicholasdegrate@gmail.com>
2021-01-26 05:01:11 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,4 +2,5 @@
 def calculator(first_number, second_number)
   sum = first_number.to_i + second_number.to_i
   difference = first_number.to_i - second_number.to_i
-  
+  product = first_number.to_i * second_number.to_i
+  qu

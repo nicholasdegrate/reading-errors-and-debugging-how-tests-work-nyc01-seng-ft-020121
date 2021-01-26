f88c8f9372fc0a2b261fd380c460f138b0f29373
Nicholas Degrate <nicholasdegrate@gmail.com>
2021-01-26 05:08:24 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,16 @@
 # Add your variables here
+first_number = 19
+second_number = 10
+
+sum = first_number.to_i + second_number.to_i
+
+
+
+  difference = first_number.to_i - second_number.to_i
+
+
+ 
+  product = first_number.to_i * second_number.to_i
+ 
+
+  quotient = first_number.to_i / second_number.to_i

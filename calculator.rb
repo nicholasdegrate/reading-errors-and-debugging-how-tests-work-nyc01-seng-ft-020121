4dabd67446a329dd207ae53794e9a7da912fc9c2
Nicholas Degrate <nicholasdegrate@gmail.com>
2021-01-26 05:08:10 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,17 +5,12 @@ second_number = 10
 sum = first_number.to_i + second_number.to_i
 
 
-def calculator2(first_number = 1, second_number)
 
   difference = first_number.to_i - second_number.to_i
 
-end
-def calculator3(first_number = 1, second_number)
+
  
   product = first_number.to_i * second_number.to_i
  
-end
-def calculator4(first_number = 1, second_number)
 
   quotient = first_number.to_i / second_number.to_i
-end

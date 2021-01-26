fb3e0254d37f89e922bf46fdd065d25896f01152
Nicholas Degrate <nicholasdegrate@gmail.com>
2021-01-26 05:05:24 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,20 +4,16 @@ def calculator(first_number, second_number)
 end
 
 def calculator2(first_number, second_number)
-  sum = first_number.to_i + second_number.to_i
+
   difference = first_number.to_i - second_number.to_i
-  product = first_number.to_i * second_number.to_i
-  quotient = first_number.to_i / second_number.to_i
+
 end
 def calculator3(first_number, second_number)
-  sum = first_number.to_i + second_number.to_i
-  difference = first_number.to_i - second_number.to_i
+ 
   product = first_number.to_i * second_number.to_i
-  quotient = first_number.to_i / second_number.to_i
+ 
 end
 def calculator4(first_number, second_number)
-  sum = first_number.to_i + second_number.to_i
-  difference = first_number.to_i - second_number.to_i
-  product = first_number.to_i * second_number.to_i
+
   quotient = first_number.to_i / second_number.to_i
 end

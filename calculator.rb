b58c89c38f319626fe5c3f23a473a34c9fc08e2e
Nicholas Degrate <nicholasdegrate@gmail.com>
2021-01-26 05:06:27 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,19 +1,19 @@
 # Add your variables here
-def calculator(first_number, second_number)
+def calculator(first_number= 1, second_number)
   sum = first_number.to_i + second_number.to_i
 end
 
-def calculator2(first_number, second_number)
+def calculator2(first_number = 1, second_number)
 
   difference = first_number.to_i - second_number.to_i
 
 end
-def calculator3(first_number, second_number)
+def calculator3(first_number = 1, second_number)
  
   product = first_number.to_i * second_number.to_i
  
 end
-def calculator4(first_number, second_number)
+def calculator4(first_number = 1, second_number)
 
   quotient = first_number.to_i / second_number.to_i
 end
